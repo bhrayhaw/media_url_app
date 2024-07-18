cb705b46682479e485423a920b68f6dd8787bdb3
Elijah Apreko <elijahapreko@gmail.com>
2024-07-18 13:00:58 +0000

    chore: Update Upload component to display URLs

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -1,32 +1,56 @@
-import React, { useState, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, useEffect } from "react";
 import API from "../services/api";
 
 const Upload: React.FC = () => {
   const [file, setFile] = useState<File | null>(null);
-  const [url, setUrl] = useState<string>("");
+  const [urls, setUrls] = useState<string[]>([]);
   const [msg, setMsg] = useState<string>("No file uploaded");
 
   const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       setFile(e.target.files[0]);
+      setMsg("");
     }
   };
 
   const handleUpload = async () => {
-    if (!file) return;
-
+    if (!file) {
+      setMsg("No file Selected for upload");
+      return;
+    }
     const formData = new FormData();
     formData.append("file", file);
 
     try {
-      const res = await API.post("/api/upload", formData);
-      setMsg('File uploaded successfully');
-      setUrl(res.data.url);
+      const res = await API.post("/api/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+      if (res.data.url) {
+        setUrls([...urls, res.data.url]);
+        setMsg("File uploaded successfully");
+      } else {
+        setMsg("Error uploading file");
+      }
     } catch (error) {
-      setMsg('Error uploading file');
+      setMsg("Error uploading file");
       console.error("Error uploading file:", error);
     }
   };
+
+  useEffect(() => {
+    const fetchUrl = async () => {
+      try {
+        const res = await API.get("/api/urls");
+        setUrls(res.data.url);
+      } catch (error) {
+        console.error("Failed to fetch URL:", error);
+      }
+    };
+
+    fetchUrl();
+  }, []);
 
   return (
     <div className="container mx-auto p-4">
@@ -37,11 +61,7 @@ const Upload: React.FC = () => {
       >
         Upload
       </button>
-      {url && (
-        <p>
-          Uploaded URL: <a href={url}>{url}</a>
-        </p>
-      )}
+      <ul>{urls && urls.map((url, index) => <li key={index}>{url}</li>)}</ul>
       {msg && <p>{msg}</p>}
     </div>
   );
